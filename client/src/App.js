b2bb193dcdda364657106744cf773411bf0b733d
FTInterritus7624 <78276637+FranklinTrinh7624@users.noreply.github.com>
2022-11-13 12:01:57 -0600

    test

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ import Customers from './components/customers';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="App Test">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">React Express Starter</h1>
