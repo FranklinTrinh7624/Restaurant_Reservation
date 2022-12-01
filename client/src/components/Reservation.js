aeb08745305b10def39e51c62fc69c19c50a229d
LongNguyen-git <lucasnguyen2112@gmail.com>
2022-12-01 05:45:16 -0600

    Reservation page

--- client/src/components/Reservation.js
+++ client/src/components/Reservation.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react'
 import Navbar from './Navbar'
+import './styles/Reservation.css'
+import axios from 'axios'
+import Input from './Input'
+import Table from './Table'
+import { Link } from 'react-router-dom'
 
 class Reservation extends Component {
+    constructor (props) {
+        super(props)
+        this.state = {
+            isLoggedIn: false,
+            tables: [
+                {id: "1", seats: "2"},
+                {id: "2", seats: "4"},
+                {id: "3", seats: "2"},
+                {id: "4", seats: "6"},
+                {id: "5", seats: "4"},
+                {id: "6", seats: "2"},
+                {id: "7", seats: "6"},
+                {id: "8", seats: "2"},
+                {id: "9", seats: "6"},
+                {id: "10", seats: "2"},
+                {id: "11", seats: "2"}
+            ],
+            tableSelected: '',
+            registerPrompt: false
+        }
+    }
+
+    componentDidMount = () => {
+        axios({
+            method: "GET",
+            withCredentials: true,
+            url: "http://localhost:5000/isAuth",
+        }).then((res) => {
+            if(res.data.error){
+                //alert(res.data.error);
+            }
+            else {
+                this.setState(prev => ({...prev, isLoggedIn: true}))
+            }
+        });
+    }
+
+    handleSubmit = () => {
+        //alert(`Table ${this.state.tableSelected} Picked`)
+        if (!this.state.isLoggedIn && !this.state.registerPrompt && this.state.tableSelected !== '') {this.setState(prev => ({...prev, registerPrompt: true}))}
+        // send to backend the table id of the selected table
+    }
+
     render() {
         return (
             <div className='Reservation'>
-                <Navbar loginState={true}/>
+                <Navbar loginState={true} transparentbg={true}/>
+                <div className='tables-container'>
+                    <div className='tables'>
+                        {this.state.tables.map((t, i) => {
+                            return <Table seatAmount={t.seats} id={t.id} key={i} selected={this.state.tableSelected} data={d => this.setState(prev => ({...prev, tableSelected: d}))}/>
+                        })}
+                    </div>
+                    <button onClick={this.handleSubmit} className='btn1'>Book Selected Table</button>
+                </div>
+                {this.state.registerPrompt ? <div className='alert-register'>
+                    <div className='alert-container'>
+                        <div>You are currently booking a guest. Do you want to register?</div>
+                        <div className='btn-group'>
+                            <Link to={'/user'} ><button className='btn2'>Yes</button></Link>
+                            <button className='btn2' onClick={() => this.setState(prev => ({...prev, registerPrompt: false}))}>No</button>
+                        </div>
+                    </div>
+                </div> : <></>}
+                <div>
+                    <form>
+                        
+                    </form>
+                </div>
             </div>
         )
     }
---
+++ client/src/components/Table.js
@@ -0,0 +1,51 @@
+import { Component } from 'react'
+import PropTypes from 'prop-types'
+
+class Table extends Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            input: false
+        }
+    }
+
+    handleOnChange = () => {
+        this.setState(prev => ({...prev, input: !prev.input}), this.sendData)
+    }
+
+    sendData = () => {
+        if (this.state.input) {this.props.data(this.props.id)}
+    }
+
+    render() {
+        var classes = ['table']
+        const seatID = this.props.id.toString()
+        classes.push(`seats${seatID}`)
+        return (
+            <div className={classes.join(' ')} onClick={this.handleOnChange} style={{backgroundColor: this.props.selected === this.props.id && this.state.input ? '#C8349B' : '#34CE9B'}}>
+                <div>Table {this.props.id}<br />{this.props.seatAmount} seats</div>
+            </div>
+            // <label>{this.props.label}:<br />
+            //     <input
+            //         disabled={this.props.disabled}
+            //         required={this.props.req}
+            //         autoComplete='off'
+            //         type={this.props.type}
+            //         value={this.props.editmode ? this.props.profilevalue : this.state.input}
+            //         onChange={this.handleOnChange}
+            //         placeholder={this.props.placeholder}/>
+            // </label>
+        )
+    }
+}
+
+Table.defaultProps = {
+    key: 0,
+    id: 0
+}
+
+Table.propTypes = {
+    
+}
+
+export default Table
